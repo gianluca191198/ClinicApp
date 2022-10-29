b809732dbb5eeef87d1faf2357f9ab1483a3c64e
Gian Luca Bellone <gbellone@inrule.com>
2022-10-29 17:13:39 -0300

    agrega lista de medicos al consultorio

--- src/Consultorio.java
+++ src/Consultorio.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public abstract class Consultorio {
 
 	public ArrayList<Prestacion> prestaciones;
+	public ArrayList<Medico> medicos;
 	
 	public void generarReporte() {}
 }
